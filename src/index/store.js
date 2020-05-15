120233f3e4ff75621903416cc5af664c1b51481c
mm <bobonpy@163.com>
2020-05-15 23:07:05 +0800

    feat: 初始化项目，配置多入口mpa

---
+++ src/index/store.js
@@ -0,0 +1,16 @@
+import {
+    createStore,
+    combineReducers,
+    applyMiddleware
+} from 'redux';
+
+import reducers from './reducers.js';
+import thunk from 'redux-thunk';
+
+export default createStore(
+    combineReducers(reducers),
+    {
+
+    },
+    applyMiddleware(thunk)
+);
---
+++ src/query/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+
+import 'normalize.css/normalize.css';
+import './index.css';
+
+import App from './App.jsx';
+import store from './store.js'
+
+ReactDom.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
